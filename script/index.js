662536858db1e6cdfa1cba941afd95c78e678784
Yejin9999 <zkdltidl@naver.com>
2025-01-12 21:09:26 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,25 @@
+//1. 키 입력하기  /값 인식속성 value
+//2. 몸무게입력하기 /값 인식속성 value
+//3. 키 몸무게 인식하는 버튼생성 / 이벤트 click
+//3-1. 버튼을 누르면 적정체중결과를 담는 결과변수
+//3-2. 결과를 p에 출력 (innerHTML)
+//적정체중 = (본인신장-100)*0.9
+//적정체중은 ?kg이며 ?kg 초과되셨습니다.
+const userHeight = document.querySelector('#userHeight');
+const userWeight = document.querySelector('#userWeight');
+const resultBtn = document.querySelector('button');
+const goal = document.querySelector('.goal')
+console.log(userHeight, userWeight, resultBtn ,goal);
+
+resultBtn.addEventListener('click',userFunc1);
+function userFunc1(){
+    let result = Number(userHeight.value)-100*0.9;
+    console.log(result);
+    goal.innerHTML = `${result} kg입니다.` ;
+}
+resultBtn.addEventListener('click',userFunc2);
+function userFunc2(){
+    let excessResult = Number(userWeight.value)-Number(userHeight.value)-100*0.9;
+    console.log(excessResult);
+    excess.innerHTML = `${excessResult} kg입니다.` ;
+}
